ab8db1768e3cc0fe59885d126bd23aec7642916f
Ozzy Osborne <ozzy@ca.ibm.com>
2016-02-01 13:56:05 -0500

    Update truststore import to add -trustcacerts param, add missing vars to default gameon.env

--- build.sh
+++ build.sh
@@ -29,7 +29,7 @@ then
   mkdir -p keystore
   keytool -genkey -alias default -storepass testOnlyKeystore -keypass testOnlyKeystore -keystore keystore/key.jks -keyalg RSA -sigalg SHA1withRSA -validity 365 -dname "CN=${IP},OU=unknown,O=unknown,L=unknown,ST=unknown,C=CA"
   keytool -export -alias default -storepass testOnlyKeystore -keypass testOnlyKeystore -keystore keystore/key.jks -file keystore/public.crt
-  keytool -import -noprompt -alias default -storepass truststore -keypass truststore -keystore keystore/truststore.jks -file keystore/public.crt
+  keytool -import -noprompt -trustcacerts -alias default -storepass truststore -keypass truststore -keystore keystore/truststore.jks -file keystore/public.crt
   rm -f keystore/public.crt
 fi
 
