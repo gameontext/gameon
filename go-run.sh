834d806f64eb34470a74af392eb8fe45e7d0bf07
Ozzy Osborne <bardweller@gmail.com>
2016-08-16 13:32:14 -0400

    Merge go-rebuild.sh logic into go-run.sh

--- go-run.sh
+++ go-run.sh
@@ -11,6 +11,7 @@
 # This will help start/stop Game On services
 #
 
+#set the action, default to help if none passed.
 if [ $# -lt 1 ]
 then
   ACTION=help
@@ -19,20 +20,63 @@ else
   shift
 fi
 
+#for when running from scripts..
+if [ $1 == "--nologs" ]
+then
+  NOLOGS=1
+  shift
+else
+  NOLOGS=0
+fi
+
+ALLPROJECTS="auth map mediator player proxy room webapp"
+if [ $# -lt 1 ]
+then
+  PROJECTS=$ALLPROJECTS
+elif [ $1 == "all" ] 
+then 
+  PROJECTS=$ALLPROJECTS
+else
+  PROJECTS=$@
+fi
+
+#configure docker compose command
 if [ "$(uname)" == "Darwin" ]
 then
-    COMPOSE="docker-compose"
+    COMPOSE="docker-compose -f docker-compose.yml -f platformservices.yml"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]
 then
-    COMPOSE="sudo docker-compose"
+    COMPOSE="sudo docker-compose -f docker-compose.yml -f platformservices.yml"
 else
-    COMPOSE="docker-compose"
+    COMPOSE="docker-compose -f docker-compose.yml -f platformservices.yml"
+fi
+
+#setup docker ip.
+NAME=${DOCKER_MACHINE_NAME-empty}
+IP=127.0.0.1
+if [ $NAME != "empty" ]
+then
+  IP=$(docker-machine ip $NAME)
 fi
 
 up_log() {
-    echo "${COMPOSE} up -d $@, logs will continue in the foreground."
+    #setup a8 default routes.
+    echo Setting default routes where needed
+    export A8_CONTROLLER_URL=http://${IP}:31200
+    export A8_REGISTRY_URL=http://${IP}:31300
+    for service in auth map mediator players proxy ; do curl -sS -X PUT ${A8_CONTROLLER_URL}/v1/versions/${service} -d '{"default" : "v1"}' -H "Authorization: local" -H "Content-Type: application/json"; done
+     
+    if [ $NOLOGS -eq 0 ]
+    then
+      echo "${COMPOSE} up -d $PROJECTS, logs will continue in the foreground."
+    else
+      echo "${COMPOSE} up -d $PROJECTS, logs are viewable using go-run.sh logs $PROJECTS"
+    fi
     ${COMPOSE} up -d $@
-    ${COMPOSE} logs --tail="5" -f $@
+    if [ $NOLOGS -eq 0 ] 
+    then
+      ${COMPOSE} logs --tail="5" -f $@
+    fi
 }
 
 down_rm() {
@@ -41,33 +85,71 @@ down_rm() {
     ${COMPOSE} rm $@
 }
 
-case "$ACTION" in
-  logs)
-    ${COMPOSE} logs -f $@
-  ;;
-  start|up)
-    up_log $@
-  ;;
-  stop|down)
-    echo "docker-compose stop $@"
-    ${COMPOSE} stop $@
-  ;;
-  restart)
-    down_rm $@
-    up_log $@
-  ;;
-  rebuild)
-    down_rm $@
-    echo "docker-compose build --pull $@"
-    ${COMPOSE} build --pull
-    up_log $@
-  ;;
-  rm)
-    echo "docker-compose rm $@"
-    ${COMPOSE} rm $@
-  ;; 
-  *)
+gradle_build() {
+	for project in $@
+	do
+	  echo -n "Evaluating ${project} for gradle build. :: "
+	  if [ -d "${project}" ] && [ -e "${project}/build.gradle" ]
+	  then
+		echo "Building project ${project} with gradle"
+		cd "$project"
+		../gradlew build
+		rc=$?
+		cd ..
+		if [ $rc != 0 ]
+		then
+		  echo Gradle build failed. Please investigate, GameOn is unlikely to work until the issue is resolved.
+		  exit 1
+		fi
+	  else
+		echo "No need to gradle build project ${project}"
+	  fi
+	done
+}
+
+usage() {
     echo "Actions: start|stop|restart|rebuild|rm|logs"
     echo "Use optional arguments to select one or more specific image"
+}
+
+case "$ACTION" in
+  logs)
+    ${COMPOSE} logs -f $PROJECTS
+  ;;
+  start|up)
+    up_log $PROJECTS
+  ;;
+  stop|down)
+    echo "docker-compose stop $PROJECTS"
+    ${COMPOSE} stop $PROJECTS
+  ;;
+  restart)
+    down_rm $PROJECTS
+    up_log $PROJECTS
+  ;;
+  rebuild)
+    down_rm $PROJECTS
+    echo "gradle build for $PROJECTS"
+    gradle_build $PROJECTS
+    if [ $? != 0 ]
+    then 
+      echo Gradle build of $PROJECTS failed.. please examine logs and retry as appropriate.
+      exit 3
+    fi
+    echo "docker-compose build --pull $PROJECTS"
+    ${COMPOSE} build --pull
+    if [ $? != 0 ]
+    then 
+      echo Docker build of $PROJECTS failed.. please examine logs and retry as appropriate.
+      exit 2
+    fi
+    up_log $PROJECTS
+  ;;
+  rm)
+    echo "docker-compose rm $PROJECTS"
+    ${COMPOSE} rm $PROJECTS
+  ;; 
+  *)
+	usage
   ;;
 esac
