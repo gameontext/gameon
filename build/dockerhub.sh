1e5cfba01b1ec5ddeeda405a45fc11f91ea87618
Erin Schnabel <schnabel@us.ibm.com>
2018-02-04 20:11:44 -0500

    build-time docker labels
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- build/dockerhub.sh
+++ build/dockerhub.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #
 # Travis builds: Used by submodule builds to build and publish images to dockerhub
@@ -14,14 +14,25 @@
 #  DOCKER_IMAGE
 #
 
+echo $TRAVIS_COMMIT
+
 COMMIT="${TRAVIS_COMMIT::8}"
 IMAGE="${DOCKER_REPO}/${DOCKER_IMAGE}"
 BUILD_TAG="${IMAGE}:${TRAVIS_BRANCH}-${TRAVIS_BUILD_NUMBER}"
 LATEST_TAG="${IMAGE}:latest"
 
+BUILD_DATE=`date -u +?%Y-%m-%dT%H:%M:%SZ?`
+URL=${URL-https://gameontext.org}
+GITHUB_URL=${GITHUB_URL-https://github.com/${IMAGE}}
+
 echo "Creating docker image for ${TRAVIS_COMMIT} on branch ${TRAVIS_BRANCH}"
 docker build \
-      --label "gameontext.commit=${TRAVIS_COMMIT}"  \
+      --label "org.label-schema.schema-version=1.0" \
+      --label "org.label-schema.build-date=${BUILD_DATE}" \
+      --label "org.label-schema.vcs-ref=${COMMIT}" \
+      --label "org.label-schema.url=${URL}" \
+      --label "org.label-schema.vcs-url=${GITHUB_URL}" \
+      --label "gameontext.commit=${COMMIT}"  \
       --label "gameontext.build=${BUILD_TAG}"  \
      -t ${BUILD_TAG} ${DOCKER_BUILDDIR}
 echo "Build complete ${DOCKER_IMAGE}"
