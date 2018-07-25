d02398fa9d4bcf73e9d3656f0ca34d78a5b601c9
Erin Schnabel <schnabel@us.ibm.com>
2018-07-25 14:05:36 -0400

    Istio, Iteration instructions
    Sort out bringup with Istio (incomplete metadata, but closer)
    Updated metadata for kafka (imagePullPolicy) for localdev
    Updated instructions for iteration w/ minikube and helm
    alongside docker-for-desktop
    
    rebuilt k8s scripts for smooth up/down with
    and without istio (make clearer functions and flow).
    
    Updated templates for w/ and w/o helm or istio
    
    Signed-off-by: Erin Schnabel <schnabel@us.ibm.com>

--- kubernetes/go-run.sh
+++ kubernetes/go-run.sh
@@ -12,98 +12,17 @@ source $SCRIPTDIR/k8s-functions
 # Ensure we're executing from project root directory
 cd "${GO_DIR}"
 
-GO_DEPLOYMENT=kubernetes
-COREPROJECTS="auth map mediator player proxy room swagger webapp"
-
-#set the action, default to help if none passed.
+# set the action, default to help if none passed.
 ACTION=help
 if [ $# -ge 1 ]; then
   ACTION=$1
   shift
 fi
 
-platform_up() {
-  if [ ! -f .gameontext.kubernetes ] || [ ! -f .gameontext.cert.pem ]; then
-    setup
-  else
-    check_cluster
-    get_cluster_ip
-    check_global_cert
-  fi
-
-  if [ -f .gameontext.helm ];  then
-    wrap_helm install --name go-system ./kubernetes/chart/gameon-system/
-  else
-    wrap_kubectl apply -R -f kubernetes/kubectl
-  fi
-
-  echo 'To wait for readiness: ./kubernetes/go-run.sh wait'
-  echo 'To type less: eval $(./kubernetes/go-run.sh env)'
-}
-
-platform_down() {
-  if kubectl get namespace gameon-system > /dev/null 2>&1; then
-    if [ -f .gameontext.helm ];  then
-      wrap_helm delete --purge go-system
-    else
-      wrap_kubectl delete -R -f kubernetes/kubectl
-    fi
-    wrap_kubectl delete namespace gameon-system
-  else
-    ok "gameon-system stopped"
-  fi
-}
-
-rebuild() {
-  PROJECTS=''
-
-  while [[ $# -gt 0 ]]; do
-    case "$1" in
-      all) PROJECTS="$COREPROJECTS $PROJECTS";;
-      *) PROJECTS="$1 $PROJECTS";;
-    esac
-    shift
-  done
-
-  echo "Building projects [$PROJECTS]"
-  for project in $PROJECTS
-  do
-    if [ ! -d "${project}" ]; then
-      continue
-    fi
-
-    echo
-    echo "*****"
-    cd "$project"
-
-    if [ -e "${project}/build.gradle" ]; then
-      echo "Building project ${project} with gradle"
-      ./gradlew build --rerun-tasks
-      rc=$?
-      if [ $rc != 0 ]; then
-        echo Gradle build failed. Please investigate, Game On! is unlikely to work until the issue is resolved.
-        exit 1
-      fi
-      echo "Building docker image for ${project}"
-      ./gradlew build image
-    elif [ "${project}" == "webapp" ] && [ -f build.sh ]; then
-      echo "webapp source present:  $(ls -d ${GO_DIR}/webapp/app)"
-      ./build.sh
-      ./build.sh final
-    elif [ -f ${project}/Dockerfile ]; then
-      echo "Re-building docker image for ${project}"
-      cd "$project"
-      ${DOCKER_CMD} build -t gameontext/gameon-${project} .
-    fi
-
-    cd ${GO_DIR}
-  done
-}
-
 usage() {
   echo "
   Actions:
-    setup    -- set up k8s secrets, prompt for helm
+    prepare  -- set up k8s secrets, prompt for helm
     reset    -- replace generated files (cert, config with cluster IP)
     env      -- eval-compatible commands to create aliases
     host     -- manually set host information about your k8s cluster
@@ -112,63 +31,75 @@ usage() {
     down     -- delete the gameon-system namespace
     status   -- return status of gameon-system namespace
     wait     -- wait until the game services are up and ready to play!
-
-    mini-istio -- minikube start with parameters for istio
   "
 }
 
 case "$ACTION" in
   reset)
-    reset
-    setup
+    reset_go
+    prepare
   ;;
-  setup)
-    setup
+  setup|prep)
+    prepare
   ;;
   up)
+    check_cluster_cfg
     platform_up
   ;;
   down)
+    check_cluster_cfg
     platform_down
-  ;;
-  host)
-    ingress_host
-    reset
   ;;
   rebuild)
     rebuild $@
   ;;
   status)
-    wrap_kubectl -n gameon-system get all
+    check_cluster_cfg
+    if kubectl -n gameon-system get po | grep -q mediator; then
+      wrap_kubectl -n gameon-system get all
 
-    get_cluster_ip
-    echo "
-    When ready, the game is available at https://${GAMEON_INGRESS}/
-    "
-  ;;
-  mini-istio)
-    wrap_minikube start \
-      --extra-config=controller-manager.ClusterSigningCertFile="/var/lib/localkube/certs/ca.crt" \
-      --extra-config=controller-manager.ClusterSigningKeyFile="/var/lib/localkube/certs/ca.key" \
-      --extra-config=apiserver.Admission.PluginNames=NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,DefaultTolerationSeconds,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota \
-      --kubernetes-version=v1.9.0 \
-      --memory 8192
+      echo "
+When ready, the game is available at https://${GAMEON_INGRESS}:${SECURE_INGRESS_PORT}/
+"
+    else
+      echo "You haven't started any game services"
+    fi
   ;;
   env)
     echo "alias go-run='${SCRIPTDIR}/go-run.sh';"
     echo "alias go-admin='${GO_DIR}/go-admin.sh'"
   ;;
   wait)
-    get_cluster_ip
-
+    check_cluster_cfg
     if kubectl -n gameon-system get po | grep -q mediator; then
       echo "Waiting for gameon-system pods to start"
       wait_until_ready -n gameon-system get pods
       echo ""
-      echo "Game On! You're ready to play: https://${GAMEON_INGRESS}/"
+      echo "Game On! You're ready to play: https://${GAMEON_INGRESS}:${SECURE_INGRESS_PORT}/"
     else
       echo "You haven't started any game services"
     fi
+  ;;
+  host)
+    check_cluster_cfg
+    define_ingress
+  ;;
+  k)
+    wrap_kubectl -n gameon-system $@
+  ;;
+  i)
+    wrap_istioctl -n gameon-system $@
+  ;;
+  cert)
+    check_cluster_cfg
+    create_certificate
+  ;;
+  install_istio)
+    check_cluster_cfg
+    install_istio
+  ;;
+  purge_istio)
+    purge_istio
   ;;
   *)
     usage
