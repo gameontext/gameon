c07f5ccc0935b50ad3931a65d7eb7563c0fbf43c
Erin Schnabel <ebullient@redhat.com>
2020-10-23 14:38:20 -0400

    revise ssl path

--- bin/gen-keystore.sh
+++ bin/gen-keystore.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
 
-echo "** Populating keystore volume **"
+echo "** Create key and trust stores **"
 
-# Generate keystores in ./keystore for a given IP
 if [ -z ${JAVA_HOME} ]
 then
   echo "JAVA_HOME is not set. Please set and re-run this script."
@@ -25,53 +24,111 @@ then
   exit 1
 fi
 
-if [ ! -f cert.pem ]; then
-  echo "Missing certificate"
-  exit 1
-fi
-if [ ! -f server.pem ]; then
-  echo "Missing server certificate"
-  exit 1
-fi
-if [ ! -f private.pem ]; then
-  echo "Missing key certificate"
-  exit 1
-fi
-if [ ! -f ltpa.keys ]; then
-  echo "Missing ltpa.keys"
+if [ $# -lt 2 ]; then
+  echo "Must specify two parameters:
+  1) the source directory (can be read-only, contains cert.pem)
+  2) the target directory (must be read-write)
+
+  An optional third parameter can specify additional environment checks.
+  Known values:
+    local: verify certificate/keystore volume contents for local dev
+
+  keystore.jks and truststore.jks will be created in the specified target directory.
+  The truststore should be specified as the JVM default using:
+    -Djavax.net.ssl.trustStore=${target_dir}/truststore.jks \
+    -Djavax.net.ssl.trustStorePassword=gameontext-trust
+"
+
   exit 1
 fi
 
-cp *.pem keystore
-cp *.keys keystore
+src_dir=$1
+target_dir=$2
+orig_dir=${pwd}
+local_dev=1
 
+if [ ! -f ${src_dir}/cert.pem ]; then
+  echo "Missing certificate: ${src_dir}/cert.pem"
+  exit 1
+fi
+
+if [ "$3" == "local" ]; then
+  local_dev=0
+  touch .local.volume
+
+  if [ ! -f ${src_dir}/server.pem ]; then
+    echo "Missing server certificate: ${src_dir}/server.pem"
+    exit 1
+  fi
+  if [ ! -f ${src_dir}/private.pem ]; then
+    echo "Missing key certificate: ${src_dir}/private.pem"
+    exit 1
+  fi
+  if [ ! -f ${src_dir}/ltpa.keys ]; then
+    echo "Missing ltpa.keys: ${src_dir}/ltpa.keys"
+    exit 1
+  fi
+elif [ -f ${target_dir}/.local.volume ]; then
+  echo "Using keystores created in a shared volume"
+  exit 0
+fi
+
+inspect() {
+  if [ $local_dev ] ; then
+    echo "inspect $1 $2"
+    keytool -list \
+        -keystore $1 -storepass $2 -storetype PKCS12
+    echo ""
+  fi
+}
+inspect_jks() {
+  if [ $local_dev ] ; then
+    echo "inspect $1 $2"
+    keytool -list \
+        -keystore $1 -storepass $2
+    echo ""
+  fi
+}
+
+cp ${src_dir}/*.pem ${target_dir}
+cp ${src_dir}/*.keys ${target_dir}
 
 echo "Building keystore/truststore from cert.pem"
+echo " # cd ${target_dir}"
+cd ${target_dir}
 
-echo "-converting pem to pkcs12"
+echo " # converting pem to pkcs12"
 openssl pkcs12 -passin pass:keystore -passout pass:keystore -export -out cert.pkcs12 -in cert.pem
 
-echo "-importing pem to keystore/truststore.jks"
-keytool -import -v -trustcacerts -alias default -file cert.pem -storepass truststore -keypass keystore -noprompt -keystore keystore/truststore.jks
+echo " # importing cert.pkcs12 to key.pkcs12"
+keytool -v -importkeystore -alias 1 -noprompt \
+        -srckeystore cert.pkcs12 -srckeypass keystore -srcstorepass keystore -srcstoretype PKCS12 -srcalias 1 \
+        -destkeystore key.pkcs12 -destkeypass gameontext-keys -deststorepass gameontext-keys -deststoretype PKCS12 -destalias default
 
-echo "-creating dummy key.jks"
-keytool -genkey -storepass testOnlyKeystore -keypass wefwef -keyalg RSA -alias endeca -keystore keystore/key.jks -dname CN=rsssl,OU=unknown,O=unknown,L=unknown,ST=unknown,C=CA
 
-echo "-emptying key.jks"
-keytool -delete -storepass testOnlyKeystore -alias endeca -keystore keystore/key.jks
+echo " # importing jvm truststore to server truststore"
+cacerts=$JAVA_HOME/jre/lib/security/cacerts
+if [ -e $JAVA_HOME/lib/security/cacerts ]; then
+  cacerts=$JAVA_HOME/lib/security/cacerts
+fi
 
-echo "-importing pkcs12 to key.jks"
-keytool -v -importkeystore -srcalias 1 -alias 1 -destalias default \
-  -noprompt -srcstorepass keystore -deststorepass testOnlyKeystore \
-  -srckeypass keystore -destkeypass testOnlyKeystore \
-  -srckeystore cert.pkcs12 -srcstoretype PKCS12 -destkeystore keystore/key.jks -deststoretype JKS
+keytool -importkeystore \
+        -srckeystore ${cacerts} -srcstorepass changeit \
+        -destkeystore truststore.jks -deststorepass gameontext-trust
 
-# Adding google certs to truststore
-echo | openssl s_client -showcerts -servername *.googleapis.com \
-  -connect www.googleapis.com:443 </dev/null 2>&1 | sed -ne '/BEGIN\ CERTIFICATE/,/END\ CERTIFICATE/ p' > googleca.pem
- 
-keytool -import -v -trustcacerts -alias googleca -file googleca.pem -storepass truststore -keypass keystore -noprompt -keystore keystore/truststore.jks 
+echo " # Add cert.pm to truststore"
+keytool -import -alias gameontext -v -noprompt \
+  -trustcacerts  \
+  -file cert.pem \
+  -keystore truststore.jks -storepass gameontext-trust
 
-curl https://secure.globalsign.net/cacert/Root-R2.crt > globalsign-r2.crt
-keytool -import -v -trustcacerts -alias globalsign-r2 -file globalsign-r2.crt -storepass truststore -keypass keystore -noprompt -keystore keystore/truststore.jks
+cd ${orig_dir}
+
+if [ $local_dev ]; then
+  echo "** Contents of ${target_dir}"
+  ls -al ${target_dir}
+
+  echo "** Keystore"
+  inspect ${target_dir}/key.pkcs12 gameontext-keys
+fi
 
